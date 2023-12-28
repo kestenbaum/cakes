b90e85c13e634be23f63687348bf5c23686f0e29
Oleksii Vovnenko <kespa71for@gmail.com>
2023-12-28 06:06:04 +0100

    add: interface Header style: add props background

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -4,12 +4,17 @@ import Logo from "../Logo/Logo.tsx";
 import {Menu, Navigate} from "./index.ts";
 import style from "./Header.module.css"
 import BurgerMenu from "../BurgerMenu/BurgerMenu.tsx";
+import {IHeader} from "../../models";
 
 
-const Header:FC = () => {
+const Header:FC<IHeader> = ({background}) => {
+
     return (
         <header
             className={style.header}
+            style = {{
+                background: background
+            }}
         >
             <div className="container">
                 <div className={style.wrapper}>
