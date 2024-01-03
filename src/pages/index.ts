2cc6a0ba957b3892e4b1a7280e7eafbf3057d5e4
Oleksii Vovnenko <kespa71for@gmail.com>
2024-01-03 04:06:57 +0100

    page: add CakesPage

--- src/pages/index.ts
+++ src/pages/index.ts
@@ -1,6 +1,7 @@
 import MainPage from "./MainPage.tsx";
 import CatalogPage from "./CatalogPage/CatalogPage.tsx";
 import Test from "./Test.tsx";
+import CakesPage from "./CakesPage/CakesPage.tsx";
 
 
-export {MainPage, CatalogPage, Test}
+export {MainPage, CatalogPage, Test, CakesPage}
