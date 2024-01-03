8e8ab6ec3a8e1595846bce2472f37ceca98c4ed7
Oleksii Vovnenko <kespa71for@gmail.com>
2024-01-03 04:19:51 +0100

    router: add in router SetsPage and NewsPage

--- src/pages/index.ts
+++ src/pages/index.ts
@@ -2,6 +2,7 @@ import MainPage from "./MainPage.tsx";
 import CatalogPage from "./CatalogPage/CatalogPage.tsx";
 import Test from "./Test.tsx";
 import CakesPage from "./CakesPage/CakesPage.tsx";
+import SetsPage from "./SetsPage/SetsPage.tsx";
+import NewsCakes from "./NewsCakes/NewsCakes.tsx";
 
-
-export {MainPage, CatalogPage, Test, CakesPage}
+export {MainPage, CatalogPage, Test, CakesPage, SetsPage, NewsCakes}
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,12 +1,14 @@
 import {AppRouter} from "../models";
 
-import {MainPage, CatalogPage, Test, CakesPage} from "../pages"
+import {MainPage, CatalogPage, Test, CakesPage, SetsPage, NewsCakes} from "../pages"
 
 export enum RouterNames {
     MAIN_PAGE = '/',
     CATALOG_PAGE = "/catalog",
     TEST = '/catalog/test',
-    CAKES_PAGE = '/catalog/cakes'
+    CAKES_PAGE = '/catalog/cakes',
+    SETS_PAGE = '/catalog/sets',
+    NEWS_CAKES = '/catalog/news',
 }
 
 export const PrivateRouter: AppRouter[] = [
@@ -14,4 +16,6 @@ export const PrivateRouter: AppRouter[] = [
     {path: RouterNames.CATALOG_PAGE, component: CatalogPage, title: "Каталог"},
     {path: RouterNames.TEST, component: Test, title: "test"},
     {path: RouterNames.CAKES_PAGE, component: CakesPage, title: "Торты"},
+    {path: RouterNames.SETS_PAGE, component: SetsPage, title: "Наборы"},
+    {path: RouterNames.NEWS_CAKES, component: NewsCakes, title: "Новинки"},
 ]
